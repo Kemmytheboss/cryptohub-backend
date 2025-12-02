2cdd7e035e3ecc3f3824eb9ca6e851536c8e4e30
Verah Mokaya <verahmokaya475@gmail.com>
2025-12-02 13:38:35 +0300

    created profile database

--- trendycryptovibe-backend/app/models/user.py
+++ trendycryptovibe-backend/app/models/user.py
@@ -1,10 +1,12 @@
 from sqlalchemy import column, Integer, String
+from sqlalchemy.orm import relationship
 from app.db.database import Base
 
 class User(Base)::
     __tablename__ = "users"
 
     id = column(Integer, primary_key=True, index=True)
-    username = column(String, unique=True, index=True, nullable=False)
     email = column(String, unique=True, index=True, nullable=False)
     hashed_password = column(String, nullable=False)
+
+    profile = relationship("UserProfile", back_populates="user", uselist=False)
