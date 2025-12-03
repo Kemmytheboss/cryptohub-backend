788f13a6c1270d7d965d5ffb4912f8282f1132c2
Verah Mokaya <verahmokaya475@gmail.com>
2025-12-03 17:44:49 +0300

    created prediction route

--- trendycryptovibe-backend/app/routes/predict.py
+++ trendycryptovibe-backend/app/routes/predict.py
@@ -1,4 +1,17 @@
 from app.ai.predictor import prediction
 from fastapi import APIRouter
 
-router = 
+router = APIRouter(prefix='/predict', tags["AI"])
+
+@router.post("/")
+def predict_price(prices: list[float])
+    """
+    Send a list of last market prices:
+    example:
+    {
+        "prices: [100, 103, 104, 102, 105]
+    }
+    """
+    result = siple_prediction(prices)
+    return{"signal": result}
+    
