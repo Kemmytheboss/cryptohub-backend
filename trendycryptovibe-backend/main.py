edf577e1856532c9a02d2cf7a3353eb424e63797
Verah Mokaya <verahmokaya475@gmail.com>
2025-12-03 07:07:05 +0300

    added the role router to main file

--- trendycryptovibe-backend/main.py
+++ trendycryptovibe-backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.db.database import engine, Base
 from app.routes.user import router as user_router
 from app.routes.wallet import router as wallet_router
+from app.routes.roles import router as roles_router
 
 Base.metadata.create_all(bind=engine)
 
@@ -12,3 +13,4 @@ def home():
     return {"status": "Backend running successfully!"}
 app.include_router(user_router)
 app.include_router(wallet_router)
+app.include_router(roles_router)
